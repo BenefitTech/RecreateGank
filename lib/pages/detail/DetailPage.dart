37dce5d239e90677b4774374e17eccc5e0f355c4
Eric <hjphfut@163.com>
2019-04-22 23:41:56 +0800

    完善detailPage

--- lib/pages/detail/DetailPage.dart
+++ lib/pages/detail/DetailPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'dart:convert';
 
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
@@ -10,6 +11,7 @@ import 'package:recreate_gank/model/DailyResponse.dart';
 import 'package:recreate_gank/utils/IndicatorUtils.dart';
 import 'package:recreate_gank/utils/SharedUtils.dart';
 import 'DetailListView.dart';
+import 'package:recreate_gank/pages/components/LittleWidget.dart';
 
 class DetailPage extends StatefulWidget {
   String feedType;
@@ -38,10 +40,122 @@ class _DetailPageState extends State<DetailPage> with HTTP, IndicatorFactory {
     super.initState();
 
     refreshController = RefreshController();
+
+    loadingData();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Widget buildContent(BuildContext context) {
+      if (requestError) {
+        return buildExceptionIndicator('网络请求出错了！');
+      }
+
+      if (listData == null) {
+        return Center(child: CupertinoActivityIndicator());
+      }
+
+      return SmartRefresher(
+        enablePullUp: true,
+        enablePullDown: true,
+        controller: refreshController,
+        headerBuilder: buildDefaultHeader,
+        footerBuilder: buildDefaultFooter,
+        footerConfig: RefreshConfig(),
+        onRefresh: (up) {
+          if (up) {
+            pullToRefresh();
+          } else {
+            loadingMore();
+          }
+        },
+      );
+    }
+
+    if (widget.showTitle) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.feedType),
+        ),
+        body: buildContent(context),
+      );
+    } else {
+      return Scaffold(
+        body: buildContent(context),
+      );
+    }
+  }
+
+  // 网络请求
+  getNewList(bool isLoadMore) async {
+    String url = getURL();
+    await getGankFromNet(url).then((CategoryResponse category) {
+      if (!category.error) {
+        var listData = category.results;
+        if (listData.length > 0) {
+          setState(() {
+            if (!isLoadMore) {
+              this.listData = listData;
+              SharedUtils.saveString(url, json.encode(category.toString()));
+            } else {
+              List list = List();
+              list.addAll(this.listData);
+              list.addAll(listData);
+              this.listData = list;
+            }
+          });
+        }
+      }
+
+      if (isLoadMore) {
+        refreshController.sendBack(false, RefreshStatus.idle);
+      } else {
+        refreshController.sendBack(true, RefreshStatus.completed);
+      }
+
+      return false;
+    }).catchError((error) {
+      refreshController.sendBack(true, RefreshStatus.failed);
+      return false;
+    });
+  }
+
+  // 下拉刷新
+  Future pullToRefresh() async {
+    currentPage = 1;
+    await getNewList(false);
+    return null;
+  }
+
+  // 加载更多
+  void loadingMore() {
+    currentPage++;
+    getNewList(true);
+  }
+
+  // 加载初始数据
+  void loadingData() async {
+    String url = getURL();
+    var cacheData = await SharedUtils.get(url);
+    if (cacheData != null) {
+      var userMap = json.decode(cacheData);
+      CategoryResponse categoryResponse = CategoryResponse.fromJson(userMap);
+      print('获取缓存数据成功');
+      setState(() {
+        listData = categoryResponse.results;
+      });
+    }
+
+    if (listData == null) {
+      print('缓存为空时，请求数据');
+      getNewList(false);
+    }
+  }
+
+  String getURL() {
+    var url = API.feed;
+    url += widget.feedType + '/10/' + this.currentPage.toString();
+    print('feedListURL:$url');
+    return url;
   }
 }
