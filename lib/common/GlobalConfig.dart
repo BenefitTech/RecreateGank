c7bd8647a88aeef632a0940c18769916619d4b62
Reed <hjphfut@163.com>
2019-05-30 15:31:29 +0800

    完善MyInfoCard

--- lib/common/GlobalConfig.dart
+++ lib/common/GlobalConfig.dart
@@ -16,7 +16,30 @@ class GlobalConfig {
   // 侧边栏
   static String welfare = "福利";
 
+  ///URL链接
+  static String wxGithubUrl = "https://github.com/ZQ330093887/GankWX";
+  static String flutterGithubUrl = "https://github.com/ZQ330093887/GankFlutter";
+  static String iosGithubUrl = "https://github.com/ZQ330093887/GankIOSProgect";
+  static String androidGithubUrl =
+      "https://github.com/ZQ330093887/ConurbationsAndroid";
+  static String aboutAuthorURl = "https://www.jianshu.com/u/9681f3bbb8c2";
+  static String thanksEditURl = "https://gank.io/backbone";
 
+  ///登录
+  static String loginTitle = "用GitHub登录";
+  static String loginSubTitle = "登录后可提交干货";
+  static String flutterVersion = "Flutter版";
+  static String wxVersion = "小程序版";
+  static String androidVersion = "Android版";
+  static String iosVersion = "iOS版";
+  static String android = "Android";
+  static String all = "all";
+  static String iOS = "iOS";
+  static String h5 = "前端";
+  static String push = "瞎推荐";
+  static String resource = "拓展资源";
+  static String app = "App";
+  static String video = "休息视频";
 
   // 登录
   static String githubLogin = 'Github 账号登录';
--- lib/pages/mine/MyInfoCard.dart
+++ lib/pages/mine/MyInfoCard.dart
@@ -15,15 +15,180 @@ Widget createMyInfoCard(BuildContext context) {
           decoration: BoxDecoration(
               color: Colors.black12,
               borderRadius: BorderRadius.all(Radius.circular(6.0))),
-          child: FlatButton(onPressed: (){
-            fastRoutePageWidgetBuilder(context, LoginPage());
-            
-          },),
+          child: FlatButton(
+            onPressed: () {
+              fastRoutePageWidgetBuilder(context, LoginPage());
+            },
+            child: Container(
+              child: ListTile(
+                leading: Container(
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://pic1.zhimg.com/v2-ec7ed574da66e1b495fcad2cc3d71cb9_xl.jpg"),
+                    radius: 20.0,
+                  ),
+                ),
+                title: Container(
+                  margin: EdgeInsets.only(top: 2.0),
+                  child: Text(GlobalConfig.loginSubTitle),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: (MediaQuery.of(context).size.width - 6.0) * 0.25,
+                child: FlatButton(
+                  onPressed: () {
+                    fastRouteWebPageBuilder(
+                        context,
+                        GlobalConfig.flutterVersion,
+                        GlobalConfig.flutterGithubUrl);
+                  },
+                  child: Container(
+                    height: 50.0,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          child: Text(
+                            '185',
+                            style: TextStyle(
+                                fontSize: 16.0, color: GlobalConfig.colorFont),
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            GlobalConfig.flutterVersion,
+                            style: TextStyle(
+                                fontSize: 12.0, color: GlobalConfig.colorFont),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 14.0,
+                width: 1.0,
+                decoration: BoxDecoration(
+                    border: BorderDirectional(
+                        start: BorderSide(color: Colors.black12, width: 1.0))),
+              ),
+              Container(
+                width: (MediaQuery.of(context).size.width - 6.0) * 0.25,
+                child: FlatButton(
+                  onPressed: () {
+                    fastRouteWebPageBuilder(context, GlobalConfig.wxVersion,
+                        GlobalConfig.wxGithubUrl);
+                  },
+                  child: Container(
+                    height: 50.0,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          child: Text(
+                            '17',
+                            style: TextStyle(
+                                fontSize: 16, color: GlobalConfig.colorFont),
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            GlobalConfig.wxVersion,
+                            style: TextStyle(
+                                fontSize: 12, color: GlobalConfig.colorFont),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 14.0,
+                width: 1.0,
+                decoration: BoxDecoration(
+                    border: BorderDirectional(
+                        start: BorderSide(color: Colors.black12, width: 12.0))),
+              ),
+              Container(
+                width: (MediaQuery.of(context).size.width - 6) * 0.25,
+                child: FlatButton(
+                  onPressed: () {
+                    fastRouteWebPageBuilder(
+                        context,
+                        GlobalConfig.androidVersion,
+                        GlobalConfig.androidGithubUrl);
+                  },
+                  child: Container(
+                    height: 50.0,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          child: Text(
+                            '218',
+                            style: TextStyle(
+                                fontSize: 16.0, color: GlobalConfig.colorFont),
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            GlobalConfig.androidVersion,
+                            style: TextStyle(
+                                fontSize: 12.0, color: GlobalConfig.colorFont),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 14.0,
+                width: 1.0,
+                decoration: BoxDecoration(
+                    border: BorderDirectional(
+                        start: BorderSide(color: Colors.black12, width: 1.0))),
+              ),
+              Container(
+                width: (MediaQuery.of(context).size.width - 6) * 0.25,
+                child: FlatButton(
+                  onPressed: () {
+                    fastRouteWebPageBuilder(context, GlobalConfig.iosVersion,
+                        GlobalConfig.iosGithubUrl);
+                  },
+                  child: Container(
+                    height: 50.0,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          child: Text(
+                            '33',
+                            style: TextStyle(
+                                fontSize: 16.0, color: GlobalConfig.colorFont),
+                          ),
+                        ),
+                        Container(
+                          child: Text(
+                            GlobalConfig.iosVersion,
+                            style: TextStyle(
+                                fontSize: 12.0, color: GlobalConfig.colorFont),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         )
       ],
     ),
   );
-
-
-  
 }
